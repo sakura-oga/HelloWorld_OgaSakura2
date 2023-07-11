957f99482c21faa482d1de1a95972ef4730b5455
ogasakura <ogasakura@localhost>
2023-07-11 22:46:24 +0900

    課題1 修正

---
+++ HelloWorld_OgaSakura/src/System/out/SelfIntroduction.java
@@ -0,0 +1,21 @@
+package System.out;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 氏名
+		String name = "大賀さくら";
+		// 年齢
+		int age = 24;
+		// 趣味
+		String hobby = "旅行";
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
