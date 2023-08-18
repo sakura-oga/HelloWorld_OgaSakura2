6a865aef51eb410a4595c9a6ffd76b11aa2ed7ce
ogasakura <ogasakura@localhost>
2023-08-18 16:38:53 +0900

    カリキュラムB_課題1.22

---
+++ curriculumB_1_22/src/exe_1_22/Country.java
@@ -0,0 +1,16 @@
+package exe_1_22;
+
+import pro_1_22.Japan;
+
+public class Country {
+	public static void main(String[] args) {
+		Japan name = new Japan("日本");
+		name.displayCountry();
+
+		Japan food = new Japan("寿司", "和食");
+		food.displayFood();
+
+		Japan date = new Japan();
+		date.displayDate();
+	}
+}
